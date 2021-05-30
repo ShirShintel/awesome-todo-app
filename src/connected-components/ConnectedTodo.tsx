0517ad1a091e697c0d8bfd66f6b7330b58a9bfe2
ShirShintel <shirsh@wix.com>
2021-05-30 15:52:49 +0300

    Added checkbox logic

--- src/connected-components/ConnectedTodo.tsx
+++ src/connected-components/ConnectedTodo.tsx
@@ -5,11 +5,13 @@ import Todo from '../components/Todo';
 
 const mapDispatchToProps = (dispatch:Dispatch) => {
     return {
-       deleteTodo: (id:number) => dispatch({type:"DELETE", key:id})
+       deleteTodo: (id:number) => dispatch({type:"DELETE", key:id}),
+       toggleChecked: (id:number) => dispatch({type:"CHECK", key:id}),
     }
 }
 
 const mapStateToProps = (state:State, ownProps: TodoOwnProps) => {
+    console.log(state)
     return {
         todo: state.todoList.todoList.find((item) => item.id===ownProps.id)
     }
