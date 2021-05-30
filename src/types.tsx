bb1e3d21d53eeca38a4868abde36750a1d5d7ec8
ShirShintel <shirsh@wix.com>
2021-05-30 11:48:01 +0300

    cleaned up types

--- src/types.tsx
+++ src/types.tsx
@@ -1,5 +1,43 @@
 import store  from './store'
 
+//### General Types ###
+
+export type TodoType = {
+    text: string
+    id: number
+    checked: boolean
+}
+
+//### Props ###
+
+export interface AddButtonProps {
+    stopAddingHandler: () => void;
+    addTodo: (s:string) => void;
+};
+
+export interface NewTodoButtonProps {
+    startAddingHandler: () => void;
+}
+
+export interface TodoProps {
+    todo?: TodoType;
+    deleteTodo: (id:number) => void;
+}
+
+export interface AppProps {
+    isAddingTodo: boolean
+}
+
+export interface TodosContainerProps {
+    todoList:TodoType[];
+};
+
+export interface TodoOwnProps {
+    id: number;
+}
+
+//### Connected Types ###
+
 export interface State {
     isAddingTodo: ReducerState,
     todoList: ReducerState
@@ -14,12 +52,6 @@ export interface Action {
     type: string
     value?: string
     key?: number
-  } 
-
-export type TodoType = {
-    text: string
-    id: number
-    checked: boolean
 }
 
 export type Dispatch = typeof store.dispatch; 
