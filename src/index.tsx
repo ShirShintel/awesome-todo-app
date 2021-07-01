9260133b6ab470893e3941a3e7372b46dc36ae22
ShirShintel <shirsh@wix.com>
2021-07-01 12:33:24 +0300

    fixed index format

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
- import React from 'react';
-import { Provider } from 'react-redux';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
 import store from './store';
 import ConnectedApp from './connected-containers/ConnectedApp';
+
 import './index.css';
 
 ReactDOM.render(
